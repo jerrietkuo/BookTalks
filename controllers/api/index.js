09c6714b5bf9b39cc422e815174ad197d0d4b548
Jerriet Kuo <jerrietkuo@gmail.com>
2024-07-31 15:28:10 -0400

    updated controllers/api/index.js
    added discussion_board_routes.js and searchbar-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const chatRoutes = require('./chat-routes');
+const searchRoutes = require('./searchbar-routes');
+const discussionRoutes = require('./Discussion_Board_routes');
 
 router.use('/users', userRoutes);
 router.use('/chat', chatRoutes);
+router.use('/search', searchRoutes);
+router.use('/discussion', discussionRoutes);
 
 module.exports = router;
