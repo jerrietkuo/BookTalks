b7d40b24005c759b585bfb4d9f4554f521ac33d7
Maleeha <162085935+maleehali@users.noreply.github.com>
2024-08-07 00:17:35 -0400

    Remove duplicate files except .DS_Store

--- controllers/api/searchbar-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../../controllers/searchbar');
-
-// Route for searching books
-router.get('/search', searchController.searchBooks);
-
-module.exports = router;
--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS booktalks_db;
-
-CREATE DATABASE booktalks_db;
